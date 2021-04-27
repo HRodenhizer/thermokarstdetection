df76cf5f2e0cb01883ba5b3e998903dc96e44af4
HRodenhizer <heidi.rodenhizer@yahoo.com>
2021-04-27 11:59:01 -0700

    reduce test data size

--- R/tk_ponds.R
+++ R/tk_ponds.R
@@ -3,7 +3,7 @@
 #' A raster brick of the ground surface elevation of some thermokarst ponds
 #' where the two layers are different years of data.
 #'
-#' @format A Large RasterBrick with two layers and 40,000 cells.
+#' @format A RasterBrick.
 #'
 #' @source \url{https://data.neonscience.org/data-products/DP3.30024.001}
 "tk_ponds"
