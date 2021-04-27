afc1b7637a36386a7571b718281f92fc6fc8097e
HRodenhizer <heidi.rodenhizer@yahoo.com>
2021-04-27 16:01:09 -0700

    add test for multiple cores

--- tests/testthat/test-tk_detect.R
+++ tests/testthat/test-tk_detect.R
@@ -20,6 +20,28 @@ test_that("output is of correct type when input is a RasterBrick", {
   expect_true(all(raster::getValues(output$thermokarst[[1]]) == 0 | raster::getValues(output$thermokarst[[1]]) == 1))
 })
 
+test_that("output is of correct type when input is a RasterLayer and n.cores > 1", {
+  output <- tk_detect(tk_ponds[[1]], n.cores = 2)
+  expect_type(output$cutoff.value, "double")
+  expect_type(output$radii, "double")
+  expect_s4_class(output$elev.crop, "RasterLayer")
+  expect_s4_class(output$avg.elev[[1]], "RasterLayer")
+  expect_s4_class(output$microtopography[[1]], "RasterLayer")
+  expect_s4_class(output$thermokarst[[1]], "RasterLayer")
+  expect_true(all(raster::getValues(output$thermokarst[[1]]) == 0 | raster::getValues(output$thermokarst[[1]]) == 1))
+})
+
+test_that("output is of correct type when input is a RasterBrick and n.cores > 1", {
+  output <- tk_detect(tk_ponds, n.cores = 2)
+  expect_type(output$cutoff.value, "double")
+  expect_type(output$radii, "double")
+  expect_s4_class(output$elev.crop, "RasterBrick")
+  expect_s4_class(output$avg.elev[[1]], "RasterBrick")
+  expect_s4_class(output$microtopography[[1]], "RasterBrick")
+  expect_s4_class(output$thermokarst[[1]], "RasterBrick")
+  expect_true(all(raster::getValues(output$thermokarst[[1]]) == 0 | raster::getValues(output$thermokarst[[1]]) == 1))
+})
+
 test_that("multiple radii give same result as single radius", {
   output1 <- tk_detect(tk_ponds[[1]])
   output2 <- tk_detect(tk_ponds[[1]], radii = c(10, 15))
